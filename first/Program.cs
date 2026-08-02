bb7c57332db987af8de1fcdd067e99c244e8b23f
Jeyi Adole <meetjeyvers@gmail.com>
2026-08-02 22:33:45 +0100

    a week ago I started

---
+++ first/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("What number time table do you need? ");
+string? input = Console.ReadLine();
+
+
+
+
+
+if(int.TryParse(input, out int number ))
+{
+    
+Console.WriteLine($"Here is your {number} multiplication table:");
+for(int i = 1; i<= 12; i++)
+{
+   Console.WriteLine($"{number} x {i} = {i * number}");
+}
+} else
+{
+    Console.WriteLine("please enter a valid number");
+}
